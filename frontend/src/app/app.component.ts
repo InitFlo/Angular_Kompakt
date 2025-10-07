296ff3355c8f516c37434dc224d03a48ef7dc3a7
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-07 13:03:00 +0200

    Footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>{{ title }}</h1>
-  `
+    <app-footer></app-footer>
+  `,
+  imports: [FooterComponent]
 })
 export class AppComponent {
   title = 'frontend';
