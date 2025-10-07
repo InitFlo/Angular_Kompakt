166b15eae7ccc2195b1ca0b64f8ee0ea2569152e
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-07 13:10:47 +0200

    navigation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   template: `
+    <app-navigation></app-navigation>
     <h1>{{ title }}</h1>
     <app-footer></app-footer>
   `,
-  imports: [FooterComponent]
+  imports: [FooterComponent, NavigationComponent]
 })
 export class AppComponent {
   title = 'frontend';
