d26dc4da969016970408c98c8321221ac928180c
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-07 13:43:16 +0200

    default route

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./components/footer/footer.component";
 import { NavigationComponent } from "./components/navigation/navigation.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   template: `
 <app-navigation></app-navigation>
 <div class="container my-4 style = margin-buttom: 80px">
-  <h1>{{ title }}</h1>
+  <h1 class ="display-3">{{ title }}</h1>
+  <router-outlet></router-outlet>
 </div>
 <app-footer></app-footer>
   `,
-  imports: [FooterComponent, NavigationComponent]
+  imports: [FooterComponent, NavigationComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'frontend';
