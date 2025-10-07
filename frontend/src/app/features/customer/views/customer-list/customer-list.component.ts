25e48c653924d39bd4c5c58a86e8a8e6692c8d35
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-07 15:11:38 +0200

    Kunden Demo-Daten

--- frontend/src/app/features/customer/views/customer-list/customer-list.component.ts
+++ frontend/src/app/features/customer/views/customer-list/customer-list.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import type { Customer } from '../../model/customer';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-customer-list',
-  imports: [],
+  imports: [
+   // JsonPipe
+   CommonModule
+  ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss'
 })
 export class CustomerListComponent {
-
+    
+  public customers: Customer[] = [
+    {
+      id: 3,
+      name : 'Tim',
+      credit_limit: 300
+    },
+    {
+      id: 4,
+      name: 'Tom',
+      credit_limit: 50
+    }
+  ]
 }
