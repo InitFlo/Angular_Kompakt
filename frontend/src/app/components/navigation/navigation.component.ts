fbec28a01fda8486435b928ec2e76d4d7c233804
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-07 16:34:13 +0200

    routerLinkActive

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   imports: [
     RouterLink,
-  ],
+    RouterLinkActive
+],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
