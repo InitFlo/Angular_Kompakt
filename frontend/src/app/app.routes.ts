0bc3c3a5faa0038daf2bdb35e913aa3b0ccf78f3
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-07 13:48:34 +0200

    StartPage

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import { DefaultPageComponent } from './pages/default-page/default-page.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/start',
+        pathMatch: 'full'
+    },
+    {
+        path: 'start',
+        component: StartPageComponent
+    },
     {
         path: '**',
         component: DefaultPageComponent
