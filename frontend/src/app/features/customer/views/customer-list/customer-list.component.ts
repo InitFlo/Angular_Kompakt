7095d5b3bf9ca764480ad321de3e79c97ff43eb8
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-08 11:40:43 +0200

    LoadingIndicator Animation

--- frontend/src/app/features/customer/views/customer-list/customer-list.component.ts
+++ frontend/src/app/features/customer/views/customer-list/customer-list.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import type { Customer } from '../../model/customer';
 import { CommonModule, JsonPipe } from '@angular/common';
 import { CustomerService } from '../../services/customer.service';
+import { LoadingIndicatorComponent } from "../../../../components/loading-indicator/loading-indicator.component";
 
 @Component({
   selector: 'app-customer-list',
   imports: [
-   // JsonPipe
-   CommonModule
-  ],
+    // JsonPipe
+    CommonModule,
+    LoadingIndicatorComponent
+],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss'
 })
