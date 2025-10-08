c2a0bf0c52de1705dd6ac48d5a3fd33f8dd738bf
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-08 11:06:28 +0200

    Unit-Test für enviroment.development

---
+++ frontend/src/environments/environment.development.spec.ts
@@ -0,0 +1,7 @@
+import { environment } from "./environment.development"
+
+describe("environment development", () => {
+    it("should have api", () => {
+        expect(environment.apiUrl).toBeTruthy();
+    })
+})
