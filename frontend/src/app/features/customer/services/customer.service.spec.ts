8379f98b9b7623a331c50d27015ec9b7767c0f97
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-08 16:13:59 +0200

    CustomerService Teste

--- frontend/src/app/features/customer/services/customer.service.spec.ts
+++ frontend/src/app/features/customer/services/customer.service.spec.ts
@@ -2,12 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { CustomerService } from './customer.service';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { firstValueFrom } from 'rxjs';
 
 describe('CustomerService', () => {
   let service: CustomerService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
+
     TestBed.configureTestingModule({
       providers: [
         provideHttpClient(),
@@ -15,9 +18,54 @@ describe('CustomerService', () => {
       ]
     });
     service = TestBed.inject(CustomerService);
+    httpTesting = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('getAll', () => {
+    it('should exist', () => {
+      expect(service.getAll).toBeTruthy();
+    })
+
+    it('should call get', async () => {
+      // $ bedeutet Observable
+      const customers$ = service.getAll();
+      const customersPromise = firstValueFrom(customers$);
+
+      const url = 'http://localhost:3001/customers/'
+
+      // Der Aufruf, wie er ausgesehen hätte
+      const req = httpTesting.expectOne(url);
+      expect(req.request.method).toBe('GET');
+
+      // mock-Daten 
+      req.flush([]);
+
+      expect(await customersPromise).toEqual([])
+
+      httpTesting.verify();
+
+    })
+
+    it('should call delete', async () => {
+      // $ bedeutet Observable
+      const deleteId = 1;
+
+      service.deleteById(deleteId).subscribe();
+
+      const url = 'http://localhost:3001/customers/' + deleteId;
+
+      // Der Aufruf, wie er ausgesehen hätte
+      const req = httpTesting.expectOne(url);
+      expect(req.request.method).toBe('DELETE');
+
+      // mock-Daten 
+      req.flush({});
+      httpTesting.verify();
+
+    })
+  })
 });
