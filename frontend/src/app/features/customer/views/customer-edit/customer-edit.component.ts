c14c30590e4266a47400f2ad4fd2ecbcaca4ed18
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-09 09:16:32 +0200

    Customer Formular Komponente angelegt

--- frontend/src/app/features/customer/views/customer-edit/customer-edit.component.ts
+++ frontend/src/app/features/customer/views/customer-edit/customer-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CustomerFormComponent } from "../../components/customer-form/customer-form.component";
 
 @Component({
   selector: 'app-customer-edit',
-  imports: [],
+  imports: [CustomerFormComponent],
   templateUrl: './customer-edit.component.html',
   styleUrl: './customer-edit.component.scss'
 })
--- frontend/src/app/features/customer/views/customer-new/customer-new.component.ts
+++ frontend/src/app/features/customer/views/customer-new/customer-new.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CustomerFormComponent } from "../../components/customer-form/customer-form.component";
 
 @Component({
   selector: 'app-customer-new',
-  imports: [],
+  imports: [CustomerFormComponent],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
