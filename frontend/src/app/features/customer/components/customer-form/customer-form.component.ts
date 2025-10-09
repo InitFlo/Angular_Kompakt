21c16660ba44c6bce9507acc16bf5d081699377e
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-09 10:00:44 +0200

    Formbuilder und Validierung

--- frontend/src/app/features/customer/components/customer-form/customer-form.component.ts
+++ frontend/src/app/features/customer/components/customer-form/customer-form.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.scss'
 })
 export class CustomerFormComponent {
+  formHandler() {
+    const customer = this.fields.value;
+    console.log(customer);
+  }
 
+  fields = inject(FormBuilder).group({
+    name: ['', 
+      [
+      Validators.required,
+      Validators.minLength(2)
+    ]
+  ],
+    credit_limit: [0,
+      [
+      Validators.required,
+      Validators.min(0)
+      ]
+    ]
+  })
 }
