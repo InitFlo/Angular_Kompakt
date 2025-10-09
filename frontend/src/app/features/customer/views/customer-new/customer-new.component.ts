0371288f5128f270e3832bab0590d12880d75531
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-09 13:27:35 +0200

    LoadingIndicator und ErrorMessageBox beim Speichern

--- frontend/src/app/features/customer/views/customer-new/customer-new.component.ts
+++ frontend/src/app/features/customer/views/customer-new/customer-new.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CustomerFormComponent } from "../../components/customer-form/customer-form.component";
 import { Customer } from '../../model/customer';
 import { CustomerService } from '../../services/customer.service';
+import { ErrorBoxComponent } from "../../../../components/error-box/error-box.component";
+import { LoadingIndicatorComponent } from "../../../../components/loading-indicator/loading-indicator.component";
 
 @Component({
   selector: 'app-customer-new',
-  imports: [CustomerFormComponent],
+  imports: [CustomerFormComponent, ErrorBoxComponent, LoadingIndicatorComponent],
   templateUrl: './customer-new.component.html',
   styleUrl: './customer-new.component.scss'
 })
@@ -15,11 +17,21 @@ export class CustomerNewComponent {
 
   }
 
+  public saving = false;
+  public errorMessage: string | null = null;
+
   createCustomer(customer: Partial<Customer>) {
+    this.saving = true;
+    this.errorMessage = null;
     this.customerService.postOne(customer)
       .subscribe({
         next: (customer) => {
           console.log("Neu angelegt: " + customer);
+          this.saving = false;
+        },
+        error: (e: Error) => {
+          this.saving = false;
+          this.errorMessage = e.message;
         }
       })
   }
