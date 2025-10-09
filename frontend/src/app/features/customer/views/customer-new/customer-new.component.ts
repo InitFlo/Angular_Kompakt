57a82157cfa105780b802263dc295ce849786deb
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-09 13:34:18 +0200

    Weiterleitung zum Dashboard

--- frontend/src/app/features/customer/views/customer-new/customer-new.component.ts
+++ frontend/src/app/features/customer/views/customer-new/customer-new.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CustomerFormComponent } from "../../components/customer-form/customer-form.component";
 import { Customer } from '../../model/customer';
 import { CustomerService } from '../../services/customer.service';
 import { ErrorBoxComponent } from "../../../../components/error-box/error-box.component";
 import { LoadingIndicatorComponent } from "../../../../components/loading-indicator/loading-indicator.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-new',
@@ -19,6 +20,7 @@ export class CustomerNewComponent {
 
   public saving = false;
   public errorMessage: string | null = null;
+  #router = inject(Router);
 
   createCustomer(customer: Partial<Customer>) {
     this.saving = true;
@@ -28,6 +30,7 @@ export class CustomerNewComponent {
         next: (customer) => {
           console.log("Neu angelegt: " + customer);
           this.saving = false;
+          this.#router.navigate(['/customers'])
         },
         error: (e: Error) => {
           this.saving = false;
