67ab0515ebbfe25b113fe80031e198014c783e3a
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-10 10:03:58 +0200

    Auth feature group

---
+++ frontend/src/app/features/auth/store/actions/auth.actions.ts
@@ -0,0 +1,12 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { LoginData } from '../../model/login-data';
+import { User } from '../../model/user';
+
+export const AuthActions = createActionGroup({
+  source: 'Auth',
+  events: {
+    'Load Auths': props<{ data: LoginData }>(),
+    'Load Auths Success': props<{ data: User }>(),
+    'Load Auths Failure': props<{ error: unknown }>(),
+  }
+});
