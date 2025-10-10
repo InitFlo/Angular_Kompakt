ddbc869633d38d77759f4fd193e3158953213e57
InitFlo <123594807+InitFlo@users.noreply.github.com>
2025-10-10 15:11:05 +0200

    Signals Beispiel

--- frontend/src/app/pages/start-page/start-page.component.ts
+++ frontend/src/app/pages/start-page/start-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-start-page',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './start-page.component.scss'
 })
 export class StartPageComponent {
-
+  counter = signal(0);
+  
+  increment() {
+    this.counter.update((c) => ++c);
+  }
 }
